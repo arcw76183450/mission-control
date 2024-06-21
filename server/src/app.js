0b0a0a5d771cacaa50bfba8eb1541d703b49f645
Akshar Chawla <arcw76183450@MacBook-Air.local>
2024-06-21 17:28:19 +0530

    CORS middleware settings

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 app.use(express.json());
 app.use(planetsRouter);
 
