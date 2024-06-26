23e7014e4dfdc88c31ba8764942b52afde5ebb09
Akshar Chawla <arcw3450@MacBook-Air.local>
2024-06-26 13:14:40 +0530

    validation for POST /launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,17 @@ function httpGetAllLaunches(req, res){
 
 function httpAddNewLaunch(req,res){
     const launch = req.body;
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+        return res.status(400).json({
+            error: "Missing required launch properties",
+        })
+    }
     launch.launchDate = new Date(launch.launchDate);
+    if(isNaN(launch.launchDate)){
+        return res.status(400).json({
+            error: "Enter a valid date",
+        })
+    }
     addNewLaunch(launch);
     return res.status(201).json(launch);
 }
