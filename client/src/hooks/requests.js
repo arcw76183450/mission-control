6553970af68a292d1fa4fe1d341a31f8da919321
Akshar Chawla <arcw3450@MacBook-Air.local>
2024-06-26 18:08:37 +0530

    Connecting POST /launches to client

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -7,7 +7,7 @@ async function httpGetPlanets() {
 }
 
 async function httpGetLaunches() {
-  const response = await fetch(`http://${API_URL}/launches`);
+  const response = await fetch(`${API_URL}/launches`);
   const fetchedLaunches =  await response.json();
   return fetchedLaunches.sort((a,b) => {
     return a.flightNumber - b.flightNumber;
@@ -15,8 +15,20 @@ async function httpGetLaunches() {
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try{
+    return await fetch(`${API_URL}/launches`,{
+      method: "post",
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    })
+  }
+  catch(err){
+    return {
+      ok: false
+    };
+  }
 }
 
 async function httpAbortLaunch(id) {
