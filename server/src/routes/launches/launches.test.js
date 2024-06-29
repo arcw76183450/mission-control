7ec7b48bde1141df1dafada34eb9519b048fce15
Akshar Chawla <126561169+arcw76183450@users.noreply.github.com>
2024-06-29 08:00:48 +0530

    Adding tests for API endpoints (#2)

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,67 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async() => {
+        const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', /json/)
+        .expect(200);
+    });
+});
+
+describe('Test POST /launches', () => {
+
+    const completeLaunch = {
+        mission: 'USS Enterprise',
+        rocket: 'NCSS 17 Enterprise',
+        target: 'Kepler-186 f',
+        launchDate: 'January 20, 2028'
+    };
+
+    const launchWithoutDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCSS 17 Enterprise',
+        target: 'Kepler-186 f',
+    };
+
+    const launchWithInvalidDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCSS 17 Enterprise',
+        target: 'Kepler-186 f',
+        launchDate: 'hello'
+    };
+
+    test('It should respond with 201 created',async() => {
+        const response = await request(app)
+        .post('/launches')
+        .send(completeLaunch)
+        .expect('Content-Type', /json/)
+        .expect(201);
+
+        const requestDate = new Date(completeLaunch.launchDate).toISOString();
+        const responseDate = response.body.launchDate;
+        expect(responseDate).toBe(requestDate);
+        expect(response.body).toMatchObject(launchWithoutDate);
+    });
+
+    test('It should catch missing required fields', async() => {
+        const response = await request(app)
+        .post('/launches')
+        .send(launchWithoutDate)
+        .expect('Content-Type', /json/)
+        .expect(400);
+
+        expect(response.body).toStrictEqual({"error": "Missing required launch properties"});
+    });
+
+    test('It should catch invalid dates', async() => {
+        const response = await request(app)
+        .post('/launches')
+        .send(launchWithInvalidDate)
+        .expect('Content-Type', /json/)
+        .expect(400);
+
+        expect(response.body).toStrictEqual({"error": "Enter a valid date"});
+    });
+});
