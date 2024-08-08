955d90d2f9bb57c6a976ac6f7cd6bbdcce81658f
Akshar Chawla <arcw3450@MacBook-Air.local>
2024-08-08 15:29:51 +0530

    Adding tests

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,14 @@
 const http = require('http');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const path = require('path');
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
+const { mongoConnect } = require('./services/mongo');
 
-dotenv.config({path: path.resolve(__dirname,"..","..",".env")});
 const PORT = process.env.PORT || 8000;
-const MONGO_URL = process.env.MONGO_URL;
-
 const httpServer = http.createServer(app);
 
-mongoose.connection.once('open', ()=> {
-    console.log('MongoDB connection ready...');
-})
-
-mongoose.connection.on('error', (error)=> {
-    console.error(error);
-})
-
 async function startServer(){
-    await mongoose.connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    await mongoConnect();
     await loadPlanetsData();
     httpServer.listen(PORT,()=>{
         console.log(`Listening on port ${PORT}`);
